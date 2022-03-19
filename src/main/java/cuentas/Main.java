6855d4f5751af04d87b68141633c5dff3a8c1612
Agustín <Agustn@DESKTOP-IN3UUGM>
2022-03-19 21:06:37 +0100

    Ejecución correcta

--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -13,7 +13,8 @@ public class Main {
 
         operativa_cuenta(cuenta1, 0);
     }
-
+//Refactorizado
+    
     public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
         try {
             cuenta1.retirar(2300);
