a7c7dd83a0e775aea50ace1700a05cc3f30983b8
Agustín <Agustn@DESKTOP-IN3UUGM>
2022-03-24 21:19:03 +0100

    Ejecución correcta

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,36 +1,100 @@
+
 package cuentas;
+
+/**
+ * <h1>Clase cuenta</h1>
+ * 
+ * Contiene información de cada cuenta corriente
+ * 
+ * @author Agustín
+ * @version 1.0
+ * @since 24/03/2022
+ */
+
 
 public class CCuenta {
 
-
+//Atributos
+    
+    /**
+     * Nombre del titular de la cuenta
+     */
     private String nombre;
+    
+    /**
+     * Número de la cuenta corriente
+     */
     private String cuenta;
+    
+    /**
+     * Saldo que arroja la cuenta
+     */
     private double saldo;
+    
+    /**
+     * Tipo de interés que se aplica
+     */
     private double tipoInterés;
 
+    
+    /**
+     * Constructor sin parámetros
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Constructor con 4 parámetros
+     * @param nom String nombre del titular de la cuenta
+     * @param cue String número de la cuenta corriente
+     * @param sal double saldo que arroja la cuenta
+     * @param tipo double tipo de interés que se aplica
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
+        tipoInterés=tipo;
     }
-
+    
+    /**
+     * @return getSaldo
+     * @see getSaldo
+     */    
     public double estado()
     {
         return getSaldo();
     }
-
+    
+    /**
+     * Metodo que lee 
+     * @param cantidad
+     * Si se cumple la condición
+     * @throws Exception
+     * Si no se cumple la condición
+     * incrementa saldo
+     * @see setSaldo
+     * @see getSaldo
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Metodo que lee
+     * @param cantidad
+     * Si se cumple alguna condición
+     * @throws Exception
+     * Si no se cumple ninguna
+     * reduce saldo
+     * @see setSaldo
+     * @see getSaldo
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
